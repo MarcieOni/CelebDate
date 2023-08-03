3044dbb6888b94957e4c697b2aa5eb1a091d7f09
Emily <emilyonishi93@gmail.com>
2023-08-03 13:15:05 -0700

    add the settings in js 4 each of the dates n add sum styles 2 make it center n the error msg redge

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,32 @@ function hideResultsAndError() {
     document.getElementById("kether").setAttribute("class", "hidden");
     document.getElementById("emilyy").setAttribute("class", "hidden");
     document.getElementById("kimyy").setAttribute("class", "hidden");
+    document.getElementById("sry").setAttribute("class", "hidden");
 }
 
-window.onload = function {
+window.onload = function() {
     document.querySelector("form").onsubmit = function(event) {
         event.preventDefault();
         hideResultsAndError();
-        
+        const age = parseInt(document.getElementById("age").value);
+        const height = parseInt(document.getElementById("height").value);
+        const sex = document.getElementById("sex").value;
+
+        if (!age || !sex || !height || age < 18) {
+            document.getElementById("error-message").removeAttribute("class");
+        } else {
+            if (age >= 40 && sex === "f" && height >= 100) {
+                document.getElementById("logyy").removeAttribute("class");
+            } else if (age >= 25 && age <= 35 && sex === "f") {
+                document.getElementById("kether").removeAttribute("class");
+            } else if (age <= 35 && sex === "m" && height < 72 && height > 60) {
+                document.getElementById("emilyy").removeAttribute("class");
+            } else if (age <= 30 && sex === "f" && height <= 60) {
+                document.getElementById("kimyy").removeAttribute("class");
+            } else {
+                document.getElementById("sry").removeAttribute("class");
+            }
+        }
+
     }
 }
